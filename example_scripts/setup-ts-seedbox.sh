6f81ead99d81b90282a60676ef020a33f100c935
Licht-Protoss <99222474+Licht-Protoss@users.noreply.github.com>
2022-04-17 21:54:08 +0200

    Fixed bug with env variable $TS_RT_CLIENT_HOME

--- example_scripts/setup-ts-seedbox.sh
+++ example_scripts/setup-ts-seedbox.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Do only once: create volume host directories (only needed first time on host)
-mkdir -p "$RT_CLIENT_HOME"/download
+mkdir -p "$TS_RT_CLIENT_HOME"/download
 
 # Do only once: Setup rtorrent user (only needed first time on host)
 groupadd -r --gid 667 rtorrent
 useradd -r --uid 667 --gid 667 -s /usr/bin/nologin rtorrent
-chown 667:667 "$RT_CLIENT_HOME"/download
+chown 667:667 "$TS_RT_CLIENT_HOME"/download
